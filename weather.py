89e417561584543e156c9f4a0401f5507963fff2
Shashank Tiwari <87411996+Shashank04Tiwari@users.noreply.github.com>
2024-02-06 15:42:46 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ root =Tk()
 root.geometry("400x400") #size of the window by default
 root.resizable(0,0) #to make the window size fixed
 #title of our window
-root.title("Weather App - AskPython.com")
+root.title("Weather App - Shashank Tiwari")
  
  
 # ----------------------Functions to fetch and display weather info
